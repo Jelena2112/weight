29fa97b723cc0d5c1ca92e3fc064db81090b5741
Jelena2112 <dijanic@gmail.com>
2022-03-20 16:41:18 +0100

    Added homepage blade

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::view('/', 'layout');
+Route::view('/', 'homepage');
 
 Route::get('/user/{user}', [UserController::class, 'showUser'])->name('showUser');
 
